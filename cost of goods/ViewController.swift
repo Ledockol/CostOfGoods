be49c324d39b4f92c5c73c90b2f42a84a3fe310a
Ledockol <Andrey-SSK98@yandex.ru>
2025-08-03 00:34:05 +0300

    Initial Commit

---
+++ cost of goods/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  cost of goods
+//
+//  Created by Андрей Беседин on 03.08.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
